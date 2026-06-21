f081bfad499128e54afd8035d110aaf7dbaf6c8c
Michael Zhu <mzhu105@gmail.com>
2026-06-21 11:25:02 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums: number[]): number {
+        while (nums[0] > nums[nums.length - 1]) {
+            const first: number = nums.shift();
+            console.log(first)
+            nums.push(first)
+        }
+        return nums[0];
+    }
+}

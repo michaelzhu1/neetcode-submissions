772ff51a8ff4555a240f28daa1183475f29c395c
Michael Zhu <mzhu105@gmail.com>
2026-06-22 12:27:07 -0700

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let l = 0;
+        let r = nums.length - 1;
+        while (l <= r) {
+            let m = Math.floor((l + r)/2);
+            if (nums[m] === target) {
+                return m;
+            }
+            if (nums[l] <= nums[m]) {
+                if (nums[l] > target || nums[m] < target) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                if (nums[r] < target || nums[m] > target) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}

24292ba4ebb1790fda3f539da49f4efc801e0292
Michael Zhu <mzhu105@gmail.com>
2026-06-22 15:56:14 -0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let l = 0;
+        let r = nums.length;
+        while (l <= r) {
+            let mid = Math.floor((l + r)/2);
+            if (nums[mid] === target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

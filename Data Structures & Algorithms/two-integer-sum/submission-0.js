df994845e34761364d741436786f5c9b0b096159
Michael Zhu <mzhu105@gmail.com>
2026-06-26 09:50:22 -0700

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = new Map()
+        for (let i = 0; i < nums.length; i++) {
+            const complement = target - nums[i];
+            if (map.has(nums[i])) {
+                return [i, map.get(nums[i])]
+            } else {
+                map.set(complement, i)
+            }
+        }
+    }
+}

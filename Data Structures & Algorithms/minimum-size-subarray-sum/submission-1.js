d2d812666f5c308d76f67c0ba66cb2b0b830642f
Michael Zhu <mzhu105@gmail.com>
2026-07-01 17:12:20 -0700

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} nums
+     * @return {number}
+     */
+    minSubArrayLen(target, nums) {
+        let left = 0;
+        let right = 0;
+        let minLen = Infinity;
+        let currSum = nums[0];
+        while (right < nums.length) {
+            if (currSum >= target) {
+                minLen = Math.min(minLen, right - left+1);
+                currSum -= nums[left];
+                left++;
+            } else {
+                right++;
+                currSum += nums[right];
+            }
+        }
+        return minLen === Infinity ? 0 : minLen;
+    }
+}

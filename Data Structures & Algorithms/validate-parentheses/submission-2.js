12e0376e9c00a43d42f42b407b141a65e287f994
Michael Zhu <mzhu105@gmail.com>
2026-07-06 16:37:20 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if (s.length %2 !== 0) {
+            return false;
+        }
+        const dict = {"]": "[", ")": "(", "}": "{"};
+        const stack = [];
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === "[" || s[i] === "(" || s[i] === "{") {
+                stack.push(s[i]);
+            } else {
+                const lastEle = stack.pop();
+                if (dict[s[i]] !== lastEle) {
+                    return false;
+                }
+            }
+        }
+        return stack.length === 0;
+    }
+}

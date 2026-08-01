2e4b0570ca37e1e1851a08e5e85dea492de7f555
Michael Zhu <mzhu105@gmail.com>
2026-08-01 11:20:55 -0700

    Add: binary-tree-inorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-3.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    inorderTraversal(root) {
+        const result = [];
+        const stack = [];
+
+        let current = root;
+        while (current || stack.length) {
+            while (current) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.push(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+}
